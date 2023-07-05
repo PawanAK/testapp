7532dec882622fa23893a822be3a12e3d5f7838f
PawanAK <pawanajjark@gmail.com>
2023-07-05 23:24:09 +0530

    response donr

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+
+import "./App.css";
+
+const configuration = new Configuration({
+  apiKey: "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",
+});
+
+const openai = new OpenAIApi(configuration);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const chat = async (e, message) => {
+    e.preventDefault();
+
+    setIsTyping(true);
+
+    let msgs = chats;
+    msgs.push({ role: "user", content: message });
+    setChats(msgs);
+    setMessage("");
+
+    await openai
+      .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: "You are a RizzGPT. You help with busness plan writting",
+          },
+          ...chats,
+        ],
+      })
+      .then((result) => {
+        msgs.push(result.data.choices[0].message);
+        setChats(msgs);
+        setIsTyping(false);
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
-    <>
+    <main>
+      <h1>React ChatGPT App</h1>
+
+      <section>
+        {
+          chats && chats.length?(
+            chats.map((chat)=>)
+          ):""
+        }
+      </section>
+
+
+      {isTyping && (
+        <div>
+          <p>
+            <i>Typing</i>
+          </p>
+        </div>
+      )}
+
+      {errorMessage && <p>{errorMessage}</p>}
+
+      <form onSubmit={(e) => chat(e, message)}>
+        <input
+          type="text"
+          name="message"
+          value={message}
+          placeholder="Type a message"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {chats.map((chat, index) => (
+          <div key={index}>
+            <p>{chat.role === "user" ? "User: " : "Assistant: "}</p>
+            <p>{chat.content}</p>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
